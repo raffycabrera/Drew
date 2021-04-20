fcd9624b61008e8c77d262f6ac0c5add200114c7
raffycabrera <raffy_cabrera@dlsu.edu.ph>
2021-04-20 21:31:49 +0800

    FINALLY GOT THE SIGN IN TO WORK ! ! CONGRATS EVERYBODY

--- app/src/main/java/edu/dlsu/drew/MainActivity.java
+++ app/src/main/java/edu/dlsu/drew/MainActivity.java
@@ -1,24 +1,39 @@
 package edu.dlsu.drew;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
-
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "edu.dlsu.DREW.MESSAGE";
+    private static final String TAG = "EmailPassword";
+    private FirebaseAuth fAuth;
 
 
+    EditText mEmail,mPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mEmail      = findViewById(R.id.Email);
+        mPassword   = findViewById(R.id.password);
+
+        fAuth = FirebaseAuth.getInstance();
 
 
     }
@@ -31,12 +46,49 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-
     public void loginPressed(View view){
-        Intent intent = new Intent(this, MainMenu.class);
-        startActivity(intent);
+
+        final String email = mEmail.getText().toString().trim();
+        String password = mPassword.getText().toString().trim();
+
+        authenticate(email,password);
 
     }
+
+    public void updateUI(FirebaseUser user){
+
+        if (user != null) {
+            Intent intent = new Intent(this, MainMenu.class);
+            startActivity(intent);
+
+        }
+    }
+
+
+
+        private void authenticate(String email, String password) {
+            // [START sign_in_with_email]
+            fAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                Log.d(TAG, "signInWithEmail:success");
+                                FirebaseUser user = fAuth.getCurrentUser();
+                                updateUI(user);
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Log.w(TAG, "signInWithEmail:failure", task.getException());
+                                Toast.makeText(MainActivity.this, "Authentication failed.",
+                                        Toast.LENGTH_SHORT).show();
+                                updateUI(null);
+                            }
+                        }
+                    });
+            // [END sign_in_with_email]
+        }
+
 
 
 }
