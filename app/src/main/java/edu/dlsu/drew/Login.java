ac9360ff2b252b00f747c438ae18568a5fb9df97
raffycabrera <raffy_cabrera@dlsu.edu.ph>
2021-04-20 19:12:48 +0800

    loginpressed for the login button in the login java

--- app/src/main/java/edu/dlsu/drew/Login.java
+++ app/src/main/java/edu/dlsu/drew/Login.java
@@ -3,6 +3,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -20,8 +21,12 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Login extends AppCompatActivity {
+
+    private static final String TAG = "EmailPassword";
+
     EditText mEmail,mPassword;
     Button mLoginBtn,mCreateBtn;
     TextView forgotTextLink;
@@ -29,6 +34,10 @@ public class Login extends AppCompatActivity {
     FirebaseAuth fAuth;
 // did i edit  - james
     //helo -- karubin
+
+
+
+    //error message is called setError is a function i think
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,12 +50,15 @@ public class Login extends AppCompatActivity {
         mLoginBtn = findViewById(R.id.loginButton);
         mCreateBtn = findViewById(R.id.signupButton);
 
+
+        /**
         mLoginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 String email = mEmail.getText().toString().trim();
                 String password = mPassword.getText().toString().trim();
+
 
                 if(TextUtils.isEmpty(email)){
                     mEmail.setError("Email is Required.");
@@ -81,7 +93,11 @@ public class Login extends AppCompatActivity {
                 });
 
             }
-        });
+        }); **/
+
+
+
+
 
 
        mCreateBtn.setOnClickListener(new View.OnClickListener() {
@@ -147,11 +163,64 @@ public class Login extends AppCompatActivity {
     public void swapToRegister(View view){
         Intent intent = new Intent(this, Register.class);
         startActivity(intent);
+    }
 
+
+    public void updateUI(FirebaseUser account){
+
+        if(account != null){
+            Toast.makeText(this,"U Signed In successfully",Toast.LENGTH_LONG).show();
+            startActivity(new Intent(this,MainActivity.class));
+
+        }else {
+            Toast.makeText(this,"U Didnt signed in",Toast.LENGTH_LONG).show();
+        }
 
     }
 
 
+
+    public void loginPressed(View view){
+
+        String email = mEmail.getText().toString().trim();
+        String password = mPassword.getText().toString().trim();
+
+        if(TextUtils.isEmpty(email)){
+            mEmail.setError("Email is Required.");
+            return;
+        }
+
+        if(TextUtils.isEmpty(password)){
+            mPassword.setError("Password is Required.");
+            return;
+        }
+        fAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = fAuth.getCurrentUser();
+
+                            updateUI(user);
+
+
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(Login.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+                    }
+                });
+
+
+
+
+    }
 
 
 
