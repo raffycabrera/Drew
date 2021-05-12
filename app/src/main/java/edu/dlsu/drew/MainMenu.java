bfcab7e092782b9395a4f9d220e796faf72a0683
raffycabrera <raffy_cabrera@dlsu.edu.ph>
2021-05-12 21:08:46 +0800

    in mainmenu line 150 to 288 in oncreate added a way to read data from the firebase to get the data from the disasters and then set the marker with the appropriate icons and titles its all nested in each ondata change because of the async way it gets pulled

--- app/src/main/java/edu/dlsu/drew/MainMenu.java
+++ app/src/main/java/edu/dlsu/drew/MainMenu.java
@@ -1,6 +1,7 @@
 package edu.dlsu.drew;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -16,14 +17,26 @@ import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
 import androidx.core.view.GravityCompat;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.FirebaseError;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Logger;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 import org.osmdroid.bonuspack.location.NominatimPOIProvider;
 import org.osmdroid.bonuspack.location.POI;
@@ -78,11 +91,7 @@ public class MainMenu extends AppCompatActivity implements NavigationView.OnNavi
         mMapController.setZoom(7);
 
         Marker startMarker = new Marker(map);
-        startMarker.setPosition(startPoint);
-        startMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
-        map.getOverlays().add(startMarker);
-        startMarker.setIcon(getResources().getDrawable(R.drawable.ic_cloud));
-        startMarker.setTitle("Typhoon here");
+
 
 
 
@@ -112,104 +121,173 @@ public class MainMenu extends AppCompatActivity implements NavigationView.OnNavi
         //now find a way to get the longitude and lat from firebase
         //instead of hard coding make this onto a nested for loop
 
-        xylist[0][0]=12.8797;
-        xylist[0][1]=122.7740;
+
+        //put the data listener thing here in the ondatachange have it call the functions that need the data
+        //that means youll have to make a function that makes the markers
+        //i guess you will have to find another way to hide all the other marker
+
+        List<String> listID = new ArrayList<>();
+        mDatabase = FirebaseDatabase.getInstance().getReference().child("Coordinates");
 
 
-        //xylist[i][j] = event.getlongitude()
+        Query query = FirebaseDatabase.getInstance().getReference().child("Coordinates");
 
-        xylist[1][0]=11.8797;
-        xylist[1][1]=122.7740;
+        //query.orderByChild("Coordinates").equalTo("test");//Here replace title with your key which you want and replace test with value throw which search
+        //query.orderByChild("title").equalTo("test").limitToFirst(1); //If you want to only one value
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                //you get here the list of post from  DB
+                if (dataSnapshot.exists()) {
+                    int i =0;
+                    for (DataSnapshot child : dataSnapshot.getChildren()) {
 
-        xylist[2][0]=10.8797;
-        xylist[2][1]=122.7740;
-
-        xylist[3][0]=9.8797;
-        xylist[3][1]=122.7740;
-
-        xylist[4][0]=13.8797;
-        xylist[4][1]=122.7740;
-
-
-//find a way to make a counter
-        for(int i =0 ; i<xylist.length;i++)
-        {
-            if (xylist[i][0] == 0 && xylist[i][1]==0){
-                break;
-            }else{
-                GeoPoint geoPoint = new GeoPoint(xylist[i][0],xylist[i][1]);
-                geoPoints.add(geoPoint);
-                //System.out.println(i);
-            }
-        }
-
-        //System.out.println(geoPoints);
-
-        for(int i =0 ; i<geoPoints.size();i++)
-        {
-            Marker marker = new Marker(map);
-            marker.setPosition(geoPoints.get(i));
-            System.out.println(i);
-
-            //get the longitude and latitude to make a geopoint permanent
-            GeoPoint scanGeoPoint = new GeoPoint(xylist[i][0],xylist[i][1]);
+                        String postId = child.getKey(); //Post Id
+                        //add it here onto a list
+                        listID.add(postId);
+                        i++;
 
 
-            marker.setOnMarkerClickListener(new Marker.OnMarkerClickListener() {
-                @Override
-                public boolean onMarkerClick(Marker marker, MapView mapView) {
+                        List<Event> list = new ArrayList<>();
+                        for (String mPostId : listID) {
 
-                    //if (!clicked){
+                            Query query1 = FirebaseDatabase.getInstance().getReference().child("Coordinates");
 
-                    marker.showInfoWindow();
-                    mapView.getController().animateTo(marker.getPosition());
-                    NominatimPOIProvider poiProvider = new NominatimPOIProvider("OSMBonusPackTutoUserAgent");
-                    ArrayList<POI> pois = poiProvider.getPOICloseTo(scanGeoPoint, "Hospital", 25, 0.5);
-                    FolderOverlay poiMarkers = new FolderOverlay(getApplicationContext());
-                    map.getOverlays().add(poiMarkers);
-                    Drawable poiIcon = getResources().getDrawable(R.drawable.hospital_icon);
-                    for (POI poi:pois){
-                        Marker poiMarker = new Marker(map);
-                        poiMarker.setTitle(poi.mType);
-                        poiMarker.setSnippet(poi.mDescription);
-                        poiMarker.setPosition(poi.mLocation);
-                        poiMarker.setIcon(poiIcon);
+                            System.out.println(mPostId);
+                            query1.addListenerForSingleValueEvent(new ValueEventListener() {
+                                @Override
+                                public void onDataChange(DataSnapshot dataSnapshot) {
+                                    //you get here the list of post from  DB
+                                    if (dataSnapshot.exists()) {
+                                        for (DataSnapshot child : dataSnapshot.getChildren()) {
+                                            String name = (String) dataSnapshot.child(mPostId).child("name").getValue();
+                                            String longitude = (String) dataSnapshot.child(mPostId).child("longitude").getValue();
+                                            String latitude = (String) dataSnapshot.child(mPostId).child("latitude").getValue();
 
-                        poiMarkers.add(poiMarker);
+
+                                            System.out.println(name);
+                                            System.out.println(longitude);
+                                            System.out.println(latitude);
+
+
+
+                                            GeoPoint geoPoint = new GeoPoint(Double.parseDouble(latitude), Double.parseDouble(longitude));
+                                            Marker marker = new Marker(map);
+                                            marker.setPosition(geoPoint);
+
+
+                                            if (name.equals("Fire")){
+                                                marker.setIcon(getResources().getDrawable(R.drawable.fire_icon));
+                                                marker.setTitle("Fire");
+
+                                            }
+                                            else if (name.equals("Covid")){
+                                                marker.setIcon(getResources().getDrawable(R.drawable.covid_icon));
+                                                marker.setTitle("COVID");
+                                            }
+                                            else if (name.equals("Flood")){
+                                                marker.setIcon(getResources().getDrawable(R.drawable.flood_icon));
+                                                marker.setTitle("Flood");
+                                            }
+
+                                            else if (name.equals("Landslide")){
+                                                marker.setIcon(getResources().getDrawable(R.drawable.landslidewarning_icon));
+                                                marker.setTitle("Landslide");
+                                            }
+
+                                            else if (name.equals("Earthquake")){
+                                                marker.setIcon(getResources().getDrawable(R.drawable.earthquake_icon));
+                                                marker.setTitle("Earthquake");
+                                            }
+                                            else if (name.equals("Typhoon")){
+                                                marker.setIcon(getResources().getDrawable(R.drawable.typhoon_icon));
+                                                marker.setTitle("Typhoon");
+                                            }
+
+
+
+
+
+                                            marker.setOnMarkerClickListener(new Marker.OnMarkerClickListener() {
+                                                @Override
+                                                public boolean onMarkerClick(Marker marker, MapView mapView) {
+
+                                                    //if (!clicked){
+
+                                                    marker.showInfoWindow();
+                                                    mapView.getController().animateTo(marker.getPosition());
+                                                    NominatimPOIProvider poiProvider = new NominatimPOIProvider("OSMBonusPackTutoUserAgent");
+                                                    ArrayList<POI> pois = poiProvider.getPOICloseTo(geoPoint, "Hospital", 15, 0.5);
+                                                    FolderOverlay poiMarkers = new FolderOverlay(getApplicationContext());
+                                                    map.getOverlays().add(poiMarkers);
+                                                    Drawable poiIcon = getResources().getDrawable(R.drawable.hospital_icon);
+                                                    for (POI poi:pois){
+                                                        Marker poiMarker = new Marker(map);
+                                                        poiMarker.setTitle(poi.mType);
+                                                        poiMarker.setSnippet(poi.mDescription);
+                                                        poiMarker.setPosition(poi.mLocation);
+                                                        poiMarker.setIcon(poiIcon);
+
+                                                        poiMarkers.add(poiMarker);
+                                                        marker.remove(map);
+                                                        marker.setPosition(geoPoint);
+                                                    }
+                                                    long speed = 3;
+                                                    mMapController.animateTo(startPoint,9.00,speed);
+
+
+                                                    // }
+                                                    /**
+                                                     else {
+                                                     mMapController.setCenter(startPoint);
+                                                     mMapController.setZoom(7);
+                                                     //map.getOverlays().clear();
+
+                                                     }
+                                                     **/
+                                                    return true;
+                                                }});
+                                            //have some if statements here depending on what type of calamity it is
+                                            map.getOverlays().add(marker);
+                                            //trying to put marker on top
+
+                                        map.invalidate();
+
+
+                                        Event userEvent = new Event();
+                                            userEvent.setName(name);
+                                            userEvent.setLongitude(longitude);
+                                            userEvent.setLatitude(latitude);
+
+
+                                            list.add(userEvent);
+
+                                        }
+                                    }
+                                }
+
+                                @Override
+                                public void onCancelled(DatabaseError databaseError) {
+
+                                }
+                            });
+
+                        }
                     }
-                    long speed = 3;
-                    mMapController.animateTo(startPoint,9.00,speed);
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
 
 
-                    // }
-                    /**
-                     else {
-                     mMapController.setCenter(startPoint);
-                     mMapController.setZoom(7);
-                     //map.getOverlays().clear();
-
-                     }
-                     **/
-                    return true;
-                }});
-            //have some if statements here depending on what type of calamity it is
-            startMarker.setIcon(getResources().getDrawable(R.drawable.ic_cloud));
-            startMarker.setTitle("Typhoon here");
-            map.getOverlays().add(marker);
-        }
-        map.invalidate();
 
     }
 
-//this will be the function to get the longitude and latitude and return a double 2d array
-    public double [][] getData(){
-        double [][] xylist = new double[20][20];
 
-
-
-        return xylist;
-
-    }
 
     @Override
     public void onBackPressed(){
