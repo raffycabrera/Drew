7a91e9f5607e5bc8defa73cd0366c2960a471646
jamesfeliciano <james_feliciano@dlsu.edu.ph>
2021-05-21 19:20:33 +0800

    account options done, only need design @kalvin

--- app/src/main/java/edu/dlsu/drew/AccountOptions.java
+++ app/src/main/java/edu/dlsu/drew/AccountOptions.java
@@ -10,8 +10,11 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.InputType;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -23,21 +26,66 @@ import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 public class AccountOptions extends AppCompatActivity {
     Button changeEmail;
     FirebaseAuth fAuth;
     FirebaseUser user;
-    String userID;
+    TextView mAdminVerifText;
+    EditText mAdminVerif;
+    String userID,currentAccountRole, selectedRole,adminVerifPass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account_options);
         changeEmail = findViewById(R.id.changeEmailButton);
+        mAdminVerif = findViewById(R.id.adminVerif);
+        mAdminVerifText = findViewById(R.id.textView6);
         fAuth = FirebaseAuth.getInstance();
         userID = fAuth.getCurrentUser().getUid();
         user = fAuth.getCurrentUser();
+        mAdminVerif.setVisibility(View.INVISIBLE);
+        mAdminVerifText.setVisibility(View.INVISIBLE);
+        Query queryAuth = FirebaseDatabase.getInstance().getReference().child("Users").child(user.getUid());
+        queryAuth.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                 currentAccountRole= (String) snapshot.child("role").getValue();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        });
+
+        Spinner roleSpinner = findViewById(R.id.accountRole);
+        String[] accountRoleList= new String[]{"Basic", "Responder", "Administrator"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, accountRoleList);
+        roleSpinner.setAdapter(adapter);
+        roleSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+            selectedRole = roleSpinner.getSelectedItem().toString();
+                if (selectedRole.equals("Administrator")){
+                    mAdminVerif.setVisibility(View.VISIBLE);
+                    mAdminVerifText.setVisibility(View.VISIBLE);
+                }
+                else if(selectedRole.equals("Basic") || selectedRole.equals("Responder")){
+                    mAdminVerif.setVisibility(View.INVISIBLE);
+                    mAdminVerifText.setVisibility(View.INVISIBLE);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
     public void changeUserEmail(View view){
         final EditText updateEmail = new EditText(view.getContext());
@@ -55,6 +103,7 @@ public class AccountOptions extends AppCompatActivity {
                     @Override
                     public void onSuccess(Void aVoid) {
                         sendEmailVerification();
+                        FirebaseDatabase.getInstance().getReference().child("Users").child(user.getUid()).child("email").setValue(newEmail);
                         alert("Success!","Email updated and verification sent, signing out and returning to log in screen", "Ok");
                     }
                 }).addOnFailureListener(new OnFailureListener() {
@@ -109,6 +158,20 @@ public class AccountOptions extends AppCompatActivity {
             passwordResetDialog.create().show();
 
         }
+    public void changeUserRole(View view) {
+        String adminVerifPass = mAdminVerif.getText().toString().trim();
+        if (currentAccountRole.equals(selectedRole)){
+        alert("Alert!","You already have the selected role!","Ok");
+        }
+        else if (!adminVerifPass.equals("#em!er)ge&nc^2y") && selectedRole.equals("Administrator")) {
+            FirebaseDatabase.getInstance().getReference().child("Users").child(user.getUid()).child("role").setValue(selectedRole);
+            alert("Success!","Correct Administrator password, Role has been changed","Ok");
+        }
+        else{
+            FirebaseDatabase.getInstance().getReference().child("Users").child(user.getUid()).child("role").setValue(selectedRole);
+            alert("Success!","Role has been changed, signing out.","Ok");
+        }
+    }
     private void alert(String title, String message, String positiveButton ){
         android.app.AlertDialog alert = new android.app.AlertDialog.Builder(AccountOptions.this).setTitle(title).setMessage(message).setPositiveButton(positiveButton, new DialogInterface.OnClickListener() {
             @Override
