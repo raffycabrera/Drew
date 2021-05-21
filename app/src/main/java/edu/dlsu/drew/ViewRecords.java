2f8643961fb15d29676b62f5061b3408fba1a17e
raffycabrera <raffy_cabrera@dlsu.edu.ph>
2021-05-21 18:33:28 +0800

    erased some comments

--- app/src/main/java/edu/dlsu/drew/ViewRecords.java
+++ app/src/main/java/edu/dlsu/drew/ViewRecords.java
@@ -75,9 +75,6 @@ public class ViewRecords extends AppCompatActivity {
 
                                     String name = (String) child.child("name").getValue();
 
-
-                                    //HashMap longitude1 = (HashMap) child.child("longitude").getValue();
-
                                     details.add(name);
 
                                 }
@@ -91,8 +88,6 @@ public class ViewRecords extends AppCompatActivity {
 
                         });
 
-                        //have hospital details here
-                        //use for loop to go through each hospital child in the event child
 
 
 
@@ -152,46 +147,4 @@ public class ViewRecords extends AppCompatActivity {
 
     }
 
-
-    private void prepareListData() {
-
-
-/**
-        // Adding child data
-        listDataHeader.add("Top 250");
-        listDataHeader.add("Now Showing");
-        listDataHeader.add("Coming Soon..");
-
-        // Adding child data
-        List<String> top250 = new ArrayList<String>();
-        top250.add("The Shawshank Redemption");
-        top250.add("The Godfather");
-        top250.add("The Godfather: Part II");
-        top250.add("Pulp Fiction");
-        top250.add("The Good, the Bad and the Ugly");
-        top250.add("The Dark Knight");
-        top250.add("12 Angry Men");
-
-        List<String> nowShowing = new ArrayList<String>();
-        nowShowing.add("The Conjuring");
-        nowShowing.add("Despicable Me 2");
-        nowShowing.add("Turbo");
-        nowShowing.add("Grown Ups 2");
-        nowShowing.add("Red 2");
-        nowShowing.add("The Wolverine");
-
-        List<String> comingSoon = new ArrayList<String>();
-        comingSoon.add("2 Guns");
-        comingSoon.add("The Smurfs 2");
-        comingSoon.add("The Spectacular Now");
-        comingSoon.add("The Canyons");
-        comingSoon.add("Europa Report");
-
-        listDataChild.put(listDataHeader.get(0), top250); // Header, Child data
-        listDataChild.put(listDataHeader.get(1), nowShowing);
-        listDataChild.put(listDataHeader.get(2), comingSoon);
-**/
-
-
-    }
 }
