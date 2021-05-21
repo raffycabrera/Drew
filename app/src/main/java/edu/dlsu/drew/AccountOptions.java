16a0024fadaed04fe902a5a4cf23c825d707cb08
jamesfeliciano <james_feliciano@dlsu.edu.ph>
2021-05-21 18:23:46 +0800

    account options change email and password now fully functioning, change role only thing missing.

--- app/src/main/java/edu/dlsu/drew/AccountOptions.java
+++ app/src/main/java/edu/dlsu/drew/AccountOptions.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.InputType;
@@ -14,8 +15,10 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
@@ -39,24 +42,25 @@ public class AccountOptions extends AppCompatActivity {
     public void changeUserEmail(View view){
         final EditText updateEmail = new EditText(view.getContext());
         final AlertDialog.Builder updateEmailDialog = new AlertDialog.Builder(view.getContext());
-        updateEmailDialog.setTitle("Reset Password?");
-        updateEmailDialog.setMessage("New Password should be 6 characters long");
+        updateEmailDialog.setTitle("Change email?");
+        updateEmailDialog.setMessage("You will be logged out after updating your email and a new verification email will be sent to your new email.");
         updateEmailDialog.setView(updateEmail);
 
         updateEmailDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-                String newPassword = updateEmail.getText().toString();
+                String newEmail = updateEmail.getText().toString();
                 //stop
-                user.updatePassword(newPassword).addOnSuccessListener(new OnSuccessListener<Void>() {
+                user.updateEmail(newEmail).addOnSuccessListener(new OnSuccessListener<Void>() {
                     @Override
                     public void onSuccess(Void aVoid) {
-                        alert("Success!","Email Updated", "Ok");
+                        sendEmailVerification();
+                        alert("Success!","Email updated and verification sent, signing out and returning to log in screen", "Ok");
                     }
                 }).addOnFailureListener(new OnFailureListener() {
                     @Override
                     public void onFailure(@NonNull Exception e) {
-                        alert("Failure","Password not updated","Ok");
+                        alert("Failure","Email not updated","Ok");
                     }
                 });
             }
@@ -73,7 +77,7 @@ public class AccountOptions extends AppCompatActivity {
     public void changeUserPassword(View view){
     final EditText resetPassword = new EditText(view.getContext());
     final AlertDialog.Builder passwordResetDialog = new AlertDialog.Builder(view.getContext());
-    passwordResetDialog.setTitle("Reset Password?");
+    passwordResetDialog.setTitle("Reset Password? You will be logged out after updating your password.");
     passwordResetDialog.setMessage("New Password should be 6 characters long");
     resetPassword.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
     passwordResetDialog.setView(resetPassword);
@@ -86,7 +90,7 @@ public class AccountOptions extends AppCompatActivity {
                    user.updatePassword(newPassword).addOnSuccessListener(new OnSuccessListener<Void>() {
                        @Override
                        public void onSuccess(Void aVoid) {
-                        alert("Success!","Password Updated", "Ok");
+                        alert("Success!","Password Updated, signing out and returning to log in screen.", "Ok");
                        }
                    }).addOnFailureListener(new OnFailureListener() {
                        @Override
@@ -110,8 +114,24 @@ public class AccountOptions extends AppCompatActivity {
             @Override
             public void onClick(DialogInterface dialog, int x) {
                 dialog.dismiss();
+                fAuth.signOut();
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
             }
         }).create();
         alert.show();
+    }
+    private void sendEmailVerification() {
+        // Send verification email
+        // [START send_email_verification]
+        final FirebaseUser user = fAuth.getCurrentUser();
+        user.sendEmailVerification()
+                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        // Email sent
+                    }
+                });
+        // [END send_email_verification]
     }
     }
